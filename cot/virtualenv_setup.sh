472a458a03e4c5db7c13c59e7d3859ee5fdddff4
ShenranTomWang <wsr.20021128@gmail.com>
2024-11-08 13:30:01 -0500

    venv setup script for Narval

---
+++ cot/virtualenv_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=4G
+#SBATCH --time=1-00:00
+#SBATCH --account=def-lingjzhu
+
+module load StdEnv/2023
+module load python/3.10
+module load rust
+ENVDIR=/scratch/shenranw/cot     # change accordingly
+virtualenv --no-download $ENVDIR
+source $ENVDIR/bin/activate
+pip install --no-index torch transformers sentencepiece pandas plotly nbformat tqdm
+pip install transformer_lens
