28e10bfd114d26003cb0adeab142adbf7a4a2838
ShenranTomWang <wsr.20021128@gmail.com>
2024-11-08 20:55:31 -0500

    debug

--- cot/data_collection.sh
+++ cot/data_collection.sh
@@ -6,10 +6,9 @@
 #SBATCH --time=10:00:00
 #SBATCH --account=def-lingjzhu
 module load StdEnv/2023
-module load python/3.10
 module load rust
-module load cudacore/.12.2.2
-ENVDIR=/scratch/shenranw/matcha
+module load arrow
+ENVDIR=/scratch/shenranw/cot
 # virtualenv --no-download $ENVDIR
 source $ENVDIR/bin/activate
 
