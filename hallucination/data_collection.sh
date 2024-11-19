bbd50852faf7cf6d3145bc47c480f249cb2ca097
ShenranTomWang <wsr.20021128@gmail.com>
2024-11-19 00:32:12 -0500

    hallucination script, generation specifying model name

--- hallucination/data_collection.sh
+++ hallucination/data_collection.sh
@@ -3,7 +3,7 @@
 #SBATCH --gpus-per-node=a100:1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=16G
-#SBATCH --time=10:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --account=def-lingjzhu
 module load StdEnv/2023
 module load rust
@@ -12,15 +12,11 @@ ENVDIR=/scratch/shenranw/cot
 # virtualenv --no-download $ENVDIR
 source $ENVDIR/bin/activate
 
-export MODEL="gemma-2-2b"
-
-export INDEX=0
+export START_IDX=0
+export MAX_IDX=-1
+export DATASET="TruthfulQA"
+export PROBE=1
 export DATASET="fantasy_reasoning"
 
-cd /project/6080355/shenranw/CoT
-python ./generation_data_collection.py
-
-export INDEX=0
-export DATASET="com2sense"
-
-python ./generation_data_collection.py
+cd /project/6080355/shenranw/Hallucination
+python ./data_collection.py
