090d9eb019c98ff53089275aa52ae2ef543d9e49
ShenranTomWang <wsr.20021128@gmail.com>
2024-11-21 14:16:07 -0500

    truthfulQA on sockeye, QAData on narval

--- Hallucination/data_collection_narval.sh
+++ Hallucination/data_collection_narval.sh
@@ -15,19 +15,18 @@ cd /project/6080355/shenranw/Hallucination
 
 export MODEL="/project/6080355/shenranw/CoT/models/google/gemma-2-2b-it"
 
+# export DATASET="TruthfulQA"
+# export START_IDX=0
+# export END_IDX=-1
+
+# export PROBE=1
+# python ./data_collection.py
+
+# export PROBE=0
+# python ./data_collection.py
+
 export START_IDX=0
-
-export DATASET="TruthfulQA"
-export END_IDX=-1
-
-export PROBE=1
-python ./data_collection.py
-
-export PROBE=0
-python ./data_collection.py
-
 export END_IDX=1000
-
 export DATASET="QAData"
 
 export PROBE=1
--- Hallucination/data_collection_sockeye.sh
+++ Hallucination/data_collection_sockeye.sh
@@ -13,14 +13,14 @@ module load apptainer
 
 export MODEL="/home/shenranw/CoT/models/google/gemma-2-2b-it"
 cd /scratch/st-jzhu71-1/shenranw/Hallucination
-export START_IDX=0
-export END_IDX=1000
-export DATASET="QAData"
-export PROBE=1
-apptainer run --nv --home /scratch/st-jzhu71-1/shenranw/Hallucination --env XDG_CACHE_HOME=$SLURM_SUBMIT_DIR /home/shenranw/jupyter/jupyter-datascience.sif python ./data_collection.py
+# export START_IDX=0
+# export END_IDX=1000
+# export DATASET="QAData"
+# export PROBE=1
+# apptainer run --nv --home /scratch/st-jzhu71-1/shenranw/Hallucination --env XDG_CACHE_HOME=$SLURM_SUBMIT_DIR /home/shenranw/jupyter/jupyter-datascience.sif python ./data_collection.py
 
-export PROBE=0
-apptainer run --nv --home /scratch/st-jzhu71-1/shenranw/Hallucination --env XDG_CACHE_HOME=$SLURM_SUBMIT_DIR /home/shenranw/jupyter/jupyter-datascience.sif python ./data_collection.py
+# export PROBE=0
+# apptainer run --nv --home /scratch/st-jzhu71-1/shenranw/Hallucination --env XDG_CACHE_HOME=$SLURM_SUBMIT_DIR /home/shenranw/jupyter/jupyter-datascience.sif python ./data_collection.py
 
 export START_IDX=0
 export END_IDX=-1
