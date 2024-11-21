a345da1bcc3a48ae02438b1bdcec625f3088cccf
ShenranTomWang <wsr.20021128@gmail.com>
2024-11-21 14:17:20 -0500

    debug

--- Hallucination/apptainer_run/data_collection.sh
+++ Hallucination/apptainer_run/data_collection.sh
@@ -1,14 +1,14 @@
 cd /scratch/st-jzhu71-1/shenranw/Hallucination
 export START_IDX=0
-export END_IDX=1000
+# export END_IDX=1000
 
-export DATASET="QAData"
+# export DATASET="QAData"
 
-export PROBE=1
-python ./data_collection.py
+# export PROBE=1
+# python ./data_collection.py
 
-export PROBE=0
-python ./data_collection.py
+# export PROBE=0
+# python ./data_collection.py
 
 export DATASET="TruthfulQA"
 export END_IDX=-1
