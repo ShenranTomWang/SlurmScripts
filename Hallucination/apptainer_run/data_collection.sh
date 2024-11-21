814f9a53e6fc613771b7c4f4451ffd5e92bd4ad9
ShenranTomWang <wsr.20021128@gmail.com>
2024-11-21 11:23:43 -0800

    Revert "debug"
    This reverts commit a345da1bcc3a48ae02438b1bdcec625f3088cccf.

--- Hallucination/apptainer_run/data_collection.sh
+++ Hallucination/apptainer_run/data_collection.sh
@@ -1,15 +1,15 @@
 export MODEL="/home/shenranw/CoT/models/google/gemma-2-2b-it"
 cd /scratch/st-jzhu71-1/shenranw/Hallucination
 export START_IDX=0
-# export END_IDX=1000
+export END_IDX=1000
 
-# export DATASET="QAData"
+export DATASET="QAData"
 
-# export PROBE=1
-# python ./data_collection.py
+export PROBE=1
+python ./data_collection.py
 
-# export PROBE=0
-# python ./data_collection.py
+export PROBE=0
+python ./data_collection.py
 
 export DATASET="TruthfulQA"
 export END_IDX=-1
