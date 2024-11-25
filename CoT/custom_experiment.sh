79e7048ee84967a20075d7e4fecdc4468530de2d
ShenranTomWang <wsr.20021128@gmail.com>
2024-11-24 21:56:21 -0500

    script for custom experiment

---
+++ CoT/custom_experiment.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=16G
+#SBATCH --time=2:00:00
+#SBATCH --account=def-lingjzhu
+module load StdEnv/2023
+module load rust
+module load arrow
+ENVDIR=/scratch/shenranw/cot
+# virtualenv --no-download $ENVDIR
+source $ENVDIR/bin/activate
+cd /project/6080355/shenranw/CoT
+
+export MODEL="gemma-2-2b-it"
+export STREAM="attn"
+export OUTPUT="./experimental_data/{MODEL}/custom_experiment_3/"
+
+export INPUT="Let's think step by step: True or false: Following the \"stay-at-home\" order, the CEO is likely to wear slippers more often than any other shoes even when at work."
+export FILENAME="generation_cot.txt"
+python ./forward_with_hook_custom.py
+python ./generate.py
+
+export INPUT="True or false: Following the \"stay-at-home\" order, the CEO is likely to wear slippers more often than any other shoes even when at work."
+export FILENAME="generation_reg.txt"
+python ./forward_with_hook_custom.py
+python ./generate.py
