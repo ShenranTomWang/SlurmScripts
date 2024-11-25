0d6ae8cf1c7f697be44241383ab14ceb3ec95a72
ShenranTomWang <wsr.20021128@gmail.com>
2024-11-25 09:56:51 -0800

    debug

--- Hallucination/data_collection_sockeye.sh
+++ Hallucination/data_collection_sockeye.sh
@@ -14,13 +14,14 @@ module load apptainer
 export MODEL="/home/shenranw/CoT/models/google/gemma-2-2b-it"
 cd /scratch/st-jzhu71-1/shenranw/Hallucination
 export START_IDX=0
-export END_IDX=-1
+export END_IDX=800
 export DATASET="QAData"
+echo $DATASET
 export PROBE=1
 apptainer run --nv --home /scratch/st-jzhu71-1/shenranw/Hallucination --env XDG_CACHE_HOME=$SLURM_SUBMIT_DIR /home/shenranw/jupyter/jupyter-datascience.sif python ./data_collection.py
 
 export START_IDX=0
-export END_IDX=-1
+export END_IDX=800
 export DATASET="QAData"
 export PROBE=0
 apptainer run --nv --home /scratch/st-jzhu71-1/shenranw/Hallucination --env XDG_CACHE_HOME=$SLURM_SUBMIT_DIR /home/shenranw/jupyter/jupyter-datascience.sif python ./data_collection.py
