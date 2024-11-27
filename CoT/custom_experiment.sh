d6905f2a6a589b2c5e272a4200e4452dd4cf3325
ShenranTomWang <wsr.20021128@gmail.com>
2024-11-26 22:22:00 -0500

    debug

--- CoT/custom_experiment.sh
+++ CoT/custom_experiment.sh
@@ -15,10 +15,10 @@ cd /project/6080355/shenranw/CoT
 
 export MODEL="gemma-2-2b-it"
 
-export STREAM="attn"
 export OUTPUT="./experimental_data/${MODEL}/custom_experiment_3/cot/"
 export INPUT="Let's think step by step: True or false: Following the \"stay-at-home\" order, the CEO is likely to wear slippers more often than any other shoes even when at work."
 export FILENAME="generation_cot.txt"
+export STREAM="attn"
 python ./forward_with_hook_custom.py
 export STREAM="attn_scores"
 python ./forward_with_hook_custom.py
@@ -27,6 +27,7 @@ python ./generate.py
 export OUTPUT="./experimental_data/${MODEL}/custom_experiment_3/regular/"
 export INPUT="True or false: Following the \"stay-at-home\" order, the CEO is likely to wear slippers more often than any other shoes even when at work."
 export FILENAME="generation_reg.txt"
+export STREAM="attn"
 python ./forward_with_hook_custom.py
 export STREAM="attn_scores"
 python ./forward_with_hook_custom.py
