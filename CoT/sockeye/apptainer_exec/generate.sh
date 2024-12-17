75566ef9fd69d4e747930b5311227df9c3047776
ShenranTomWang <wsr.20021128@gmail.com>
2024-12-17 14:46:28 -0800

    file renaming

--- CoT/sockeye/apptainer_exec/generate.sh
+++ CoT/sockeye/apptainer_exec/generate.sh
@@ -1,7 +1,7 @@
 export HF_HOME="/scratch/st-jzhu71-1/shenranw/transformers_cache"
 export INPUT="What is 103 times 202?"
 export MODEL="hymba-1.5b-instruct"
-export FILENAME="generation_cot_2.txt"
+export FILENAME="generation_hymba_1.txt"
 
 cd /scratch/st-jzhu71-1/shenranw/CoT
 python ./generate.py
