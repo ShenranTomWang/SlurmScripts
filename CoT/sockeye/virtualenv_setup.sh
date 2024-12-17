54d596c7d96a594a77fc48b5308be84fca6fda69
ShenranTomWang <wsr.20021128@gmail.com>
2024-12-17 12:35:37 -0800

    rename, setup script

---
+++ CoT/sockeye/virtualenv_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --nodes=1  
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=16G
+#SBATCH --time=1-00:00
+#SBATCH --account=def-lingjzhu
+
+module load cuda/12.4.0 python/3.8.10 gcc
+ENVDIR=/scratch/st-jzhu71-1/shenranw/envs/CoT     # change accordingly
+virtualenv --no-download $ENVDIR
+source $ENVDIR/bin/activate
+wget --header="Authorization: Bearer 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" https://huggingface.co/nvidia/Hymba-1.5B-Instruct/resolve/main/setup.sh
+bash setup.sh
+rm setup.sh
+pip install torch sentencepiece pandas nbformat tqdm
+pip install transformer_lens
+pip install tiktoken
+pip install protobuf
+pip install ninja einops triton packaging
+pip install notebook
