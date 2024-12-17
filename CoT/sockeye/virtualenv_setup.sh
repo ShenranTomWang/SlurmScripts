66493dcdd72a123959bc60f43c9d33d8fdc35488
ShenranTomWang <wsr.20021128@gmail.com>
2024-12-17 13:11:54 -0800

    install packages before setup.sh

--- CoT/sockeye/virtualenv_setup.sh
+++ CoT/sockeye/virtualenv_setup.sh
@@ -9,12 +9,11 @@ module load cuda/12.4.0 python/3.8.10 gcc py-virtualenv
 ENVDIR=/scratch/st-jzhu71-1/shenranw/envs/CoT     # change accordingly
 virtualenv --no-download $ENVDIR
 source $ENVDIR/bin/activate
+
+pip install torch sentencepiece pandas nbformat tqdm
+pip install transformer_lens tiktoken protobuf ninja einops triton packaging
+pip install notebook
+
 wget --header="Authorization: Bearer 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" https://huggingface.co/nvidia/Hymba-1.5B-Instruct/resolve/main/setup.sh
 bash setup.sh
 rm setup.sh
-pip install torch sentencepiece pandas nbformat tqdm
-pip install transformer_lens
-pip install tiktoken
-pip install protobuf
-pip install ninja einops triton packaging
-pip install notebook
