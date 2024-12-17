b34a1775142786b662a9e68d637ae783ff746b29
ShenranTomWang <wsr.20021128@gmail.com>
2024-12-17 12:37:11 -0800

    debug

--- CoT/sockeye/virtualenv_setup.sh
+++ CoT/sockeye/virtualenv_setup.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=1-00:00
 #SBATCH --account=def-lingjzhu
 
-module load cuda/12.4.0 python/3.8.10 gcc
+module load cuda/12.4.0 python/3.8.10 gcc py-virtualenv
 ENVDIR=/scratch/st-jzhu71-1/shenranw/envs/CoT     # change accordingly
 virtualenv --no-download $ENVDIR
 source $ENVDIR/bin/activate
