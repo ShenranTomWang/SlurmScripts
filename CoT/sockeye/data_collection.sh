2b6d7565a27262d110745358b6d3b1d3e8dfab7e
ShenranTomWang <wsr.20021128@gmail.com>
2024-12-18 17:10:50 -0800

    data collection script

---
+++ CoT/sockeye/data_collection.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=my_jupyter_notebook
+#SBATCH --account=st-jzhu71-1-gpu
+#SBATCH --time=3-00:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=32G
+#SBATCH --gpus=1
+#SBATCH --constraint=gpu_mem_32
+module load cuda/12.4.0 intel-oneapi-compilers/2023.1.0 python/3.11.6 gcc
+export ENVDIR=/scratch/st-jzhu71-1/shenranw/envs/CoT     # change accordingly
+source $ENVDIR/bin/activate
+cd /scratch/st-jzhu71-1/shenranw/CoT
+
+export TRITON_CACHE_DIR="/scratch/st-jzhu71-1/shenranw/triton_cache"
+export HF_HOME="/scratch/st-jzhu71-1/shenranw/transformers_cache"
+
+export DATASET="GSM8K"
+export MAX_IDX=200
+export MODEL="hymba-1.5b-instruct"
+
+python ./generate.py
