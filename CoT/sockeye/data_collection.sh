034c219c0d92669054e7866998469a889a6d3b25
ShenranTomWang <wsr.20021128@gmail.com>
2024-12-18 17:51:18 -0800

    debug

--- CoT/sockeye/data_collection.sh
+++ CoT/sockeye/data_collection.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem=32G
 #SBATCH --gpus=1
 #SBATCH --constraint=gpu_mem_32
-module load cuda/12.4.0 intel-oneapi-compilers/2023.1.0 python/3.11.6 gcc
+module load intel-oneapi-compilers/2023.1.0 python/3.11.6 gcc
 export ENVDIR=/scratch/st-jzhu71-1/shenranw/envs/CoT     # change accordingly
 source $ENVDIR/bin/activate
 cd /scratch/st-jzhu71-1/shenranw/CoT
@@ -19,4 +19,4 @@ export DATASET="GSM8K"
 export MAX_IDX=200
 export MODEL="gemma-2-2b-it"
 
-python ./generate.py
+python ./data_collection.py
