60c0bd9af1409600424efd0683f48aa1d77f5ff3
ShenranTomWang <wsr.20021128@gmail.com>
2025-01-24 10:08:25 -0800

    refactor project

---
+++ Interpretation/sockeye/virtualenv_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1  
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=16G
+#SBATCH --time=1-00:00
+#SBATCH --account=def-jjnunez
+
+module load intel-oneapi-compilers/2023.1.0 python/3.11.6 gcc
+export ENVDIR=/scratch/st-jjnunez-1/shenranw/envs/Interpretation     # change accordingly
+python -m venv $ENVDIR
+source $ENVDIR/bin/activate
+
+cd /home/shenranw/Interpretation
+pip install -r requirements.txt
