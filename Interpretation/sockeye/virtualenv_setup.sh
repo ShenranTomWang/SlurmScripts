3f6e36694341e52d7d5972d9c4afbf357e7b57c0
ShenranTomWang <wsr.20021128@gmail.com>
2025-01-24 10:09:05 -0800

    comments

--- Interpretation/sockeye/virtualenv_setup.sh
+++ Interpretation/sockeye/virtualenv_setup.sh
@@ -8,7 +8,7 @@
 module load intel-oneapi-compilers/2023.1.0 python/3.11.6 gcc
 export ENVDIR=/scratch/st-jjnunez-1/shenranw/envs/Interpretation     # change accordingly
 python -m venv $ENVDIR
-source $ENVDIR/bin/activate
+source $ENVDIR/bin/activate     # activate the virtual environment
 
 cd /home/shenranw/Interpretation
 pip install -r requirements.txt
