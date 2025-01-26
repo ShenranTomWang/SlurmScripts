21ea37382fbfb52fda766b74276705822816ac64
ShenranTomWang <wsr.20021128@gmail.com>
2025-01-25 23:43:55 -0800

    record scripts, not tested

---
+++ ICL/virtualenv_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1  
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=16G
+#SBATCH --time=1-00:00
+#SBATCH --account=def-lingjzhu
+
+module load intel-oneapi-compilers/2023.1.0 python/3.11.6 gcc
+module load cuda/12.4.0
+export ENVDIR=/scratch/st-jzhu71-1/shenranw/envs/ICL     # change accordingly
+python -m venv $ENVDIR
+source $ENVDIR/bin/activate
+
+pip install torch sentencepiece pandas nbformat tqdm
+pip install transformer_lens tiktoken protobuf ninja einops triton packaging
+pip install notebook mamba-ssm wheel flash-attn causal-conv1d
+
+git clone https://github.com/pytorch-labs/attention-gym.git
+cd attention-gym
+pip install .
+cd ..
