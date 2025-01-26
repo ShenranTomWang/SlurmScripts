57a21d533b347c2c34f4194031f8ccab7e06fe82
ShenranTomWang <wsr.20021128@gmail.com>
2025-01-26 15:18:19 -0800

    use $MODEL to store model path

--- ICL/sockeye/test_custom.sh
+++ ICL/sockeye/test_custom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=test_custom
+#SBATCH --job-name=ICL-Hymba
 #SBATCH --account=st-jzhu71-1-gpu
-#SBATCH --time=3-00:00:00
+#SBATCH --time=7-00:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=32G
@@ -12,10 +12,11 @@ module load intel-oneapi-compilers/2023.1.0 python/3.11.6 gcc
 module load cuda/12.4.0
 export ENVDIR=/scratch/st-jzhu71-1/shenranw/envs/CoT     # change accordingly
 source $ENVDIR/bin/activate
+export MODEL="/scratch/st-jzhu71-1/shenranw/models/nvidia/Hymba-1.5B-Base"
 
 export TRITON_CACHE_DIR="/scratch/st-jzhu71-1/shenranw/triton_cache"
 export HF_HOME="/scratch/st-jzhu71-1/shenranw/transformers_cache"
 
 cd /scratch/st-jzhu71-1/shenranw/ICL
-python test_custom.py --model /scratch/st-jzhu71-1/shenranw/models/nvidia/Hymba-1.5B-Base --dataset sms_spam_random --out_dir out/Hymba-1.5B-Base/sms_spam_random --k 4
-python test_custom.py --model /scratch/st-jzhu71-1/shenranw/models/nvidia/Hymba-1.5B-Base --dataset sms_spam --out_dir out/Hymba-1.5B-Base/sms_spam --k 4
+python test_custom.py --model $MODEL --dataset sms_spam_random --out_dir out/gpt2/sms_spam_random --k 4
+python test_custom.py --model $MODEL --dataset sms_spam --out_dir out/gpt2/sms_spam --k 4
