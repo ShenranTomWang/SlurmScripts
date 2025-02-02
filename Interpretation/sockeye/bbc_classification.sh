07cc617b16b0736053787535da777849b5c0e196
ShenranTomWang <wsr.20021128@gmail.com>
2025-02-02 14:05:22 -0800

    updated script

--- Interpretation/sockeye/bbc_classification.sh
+++ Interpretation/sockeye/bbc_classification.sh
@@ -9,6 +9,9 @@ module load intel-oneapi-compilers/2023.1.0 python/3.11.6 gcc
 export ENVDIR=/scratch/st-jjnunez-1/shenranw/envs/Interpretation     # change accordingly
 python -m venv $ENVDIR
 source $ENVDIR/bin/activate     # activate the virtual environment
+export MPLCONFIGDIR=/scratch/st-jjnunez-1/shenranw/
+export TRITON_CACHE_DIR="/scratch/st-jzhu71-1/shenranw/triton_cache"
+export HF_HOME="/scratch/st-jzhu71-1/shenranw/transformers_cache"
 
-cd /home/st-jjnunez-1/shenranw/Interpretation
-pip install -r requirements.txt
+cd /scratch/st-jzhu71-1/shenranw/Interpretation
+python ./bbc_classification.py
