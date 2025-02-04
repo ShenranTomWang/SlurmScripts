8a9ba10a71c1fa7c984eb7799c69dfa999e2fe5b
ShenranTomWang <wsr.20021128@gmail.com>
2025-02-03 18:55:56 -0800

    more time for test by task

--- ICL/sockeye/test_custom_class_to_class.sh
+++ ICL/sockeye/test_custom_class_to_class.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=class_to_class
 #SBATCH --account=st-jzhu71-1-gpu
-#SBATCH --time=5:00:00
+#SBATCH --time=5-00:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem=32G
