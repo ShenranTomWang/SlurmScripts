11216024802e3cff9a8f5755b0908bc4cc97fcd0
ShenranTomWang <wsr.20021128@gmail.com>
2025-02-21 17:26:44 -0800

    refactor

--- Interpretation/sockeye/run.sh
+++ Interpretation/sockeye/run.sh
@@ -15,4 +15,11 @@ export TRITON_CACHE_DIR="/scratch/st-jzhu71-1/shenranw/triton_cache"
 export HF_HOME="/scratch/st-jzhu71-1/shenranw/transformers_cache"
 
 cd /scratch/st-jzhu71-1/shenranw/Interpretation
-python ./bbc_classification.py
+
+export CONFIG="configs/bbc_config.json"
+export MODEL="/scratch/st-jzhu71-1/shenranw/models/Umesh/distilbert-bbc-news-classification"
+python ./run.py --config $CONFIG --model $MODEL --multiclass huggingface
+
+export CONFIG="configs/topics_classification.json"
+export MODEL="/scratch/st-jzhu71-1/shenranw/models/openai-community/gpt2"
+python ./run.py --config $CONFIG --model $MODEL --multiclass llm_classifier_model --llm_classifier_config_dir "out/gpt2/config.json" --llm_classifier_dir "out/gpt2/classifier.pth"
