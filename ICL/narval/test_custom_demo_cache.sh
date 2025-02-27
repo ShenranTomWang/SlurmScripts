7377b1ff1454384e8e26a91c30aa4493ab7a7bd7
ShenranTomWang <wsr.20021128@gmail.com>
2025-02-26 20:21:59 -0500

    update config for narval

--- ICL/narval/test_custom_demo_cache.sh
+++ ICL/narval/test_custom_demo_cache.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=class_to_class
-#SBATCH --account=st-jzhu71-1-gpu
-#SBATCH --time=5-00:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --mem=32G
-#SBATCH --gpus=1
-#SBATCH --constraint=gpu_mem_32
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=16G
+#SBATCH --time=1:00:00
+#SBATCH --account=def-lingjzhu
 
 module load arrow gcc
 module load cuda
