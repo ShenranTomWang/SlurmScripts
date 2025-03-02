e705f86eb01e25e6bc8210b633e3ae68ff71fca5
ShenranTomWang <wsr.20021128@gmail.com>
2025-03-02 15:06:35 -0800

    update run script

--- Interpretation/sockeye/run.sh
+++ Interpretation/sockeye/run.sh
@@ -20,6 +20,10 @@ export CONFIG="configs/bbc_config.json"
 export MODEL="/scratch/st-jzhu71-1/shenranw/models/Umesh/distilbert-bbc-news-classification"
 python ./run.py --config $CONFIG --model $MODEL --multiclass huggingface
 
-export CONFIG="configs/topics_classification.json"
+export CONFIG="configs/topics_classification-gpt2.json"
 export MODEL="/scratch/st-jzhu71-1/shenranw/models/openai-community/gpt2"
 python ./run.py --config $CONFIG --model $MODEL --multiclass llm_classifier_model --llm_classifier_config_dir "out/gpt2/config.json" --llm_classifier_dir "out/gpt2/classifier.pth"
+
+export CONFIG="configs/topics_classification-Llama-3.2-1B.json"
+export MODEL="/scratch/st-jzhu71-1/shenranw/models/meta-llama/Llama-3.2-1B"
+python ./run.py --config $CONFIG --model $MODEL --multiclass llm_classifier_model --llm_classifier_config_dir "out/Llama-3.2-1B/config.json" --llm_classifier_dir "out/Llama-3.2-1B/classifier.pth"
