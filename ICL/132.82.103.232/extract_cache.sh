ccbac85cf50633b5ed4a168cae5bfdb8cf30f2d2
shenranw <wsr.20021128@gmail.com>
2025-03-05 22:25:12 -0800

    scripts for workstation

---
+++ ICL/132.82.103.232/extract_cache.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=a100:1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=16G
+#SBATCH --time=12:00:00
+#SBATCH --account=def-lingjzhu
+
+conda activate LLM
+
+export TRITON_CACHE_DIR="~/triton_cache"
+export HF_HOME="~/transformers_cache"
+
+cd /project/6080355/shenranw/ICL
+
+# export MODEL="~/models/openai-community/gpt2"
+# export OUT_DIR="out/gpt2"
+# python extract_activations.py --model $MODEL --add_newlines --task class_to_class --k 4 --split demo --out_dir $OUT_DIR --operator TransformerOperator --stream cache
+# python extract_activations.py --model $MODEL --add_newlines --task class_to_class_random --k 4 --split demo --out_dir $OUT_DIR --operator TransformerOperator --stream cache
+
+# export MODEL="~/models/meta-llama/Llama-3.2-1B"
+# export OUT_DIR="out/Llama-3.2-1B"
+# python extract_activations.py --model $MODEL --add_newlines --task class_to_class --k 4 --split demo --out_dir $OUT_DIR --operator TransformerOperator --stream cache
+# python extract_activations.py --model $MODEL --add_newlines --task class_to_class_random --k 4 --split demo --out_dir $OUT_DIR --operator TransformerOperator --stream cache
+
+export MODEL="~/models/Qwen/Qwen2.5-1.5B"
+export OUT_DIR="out/Qwen2.5-1.5B"
+python extract_activations.py --model $MODEL --add_newlines --task class_to_class --k 4 --split demo --out_dir $OUT_DIR --operator TransformerOperator --stream cache
+python extract_activations.py --model $MODEL --add_newlines --task class_to_class_random --k 4 --split demo --out_dir $OUT_DIR --operator TransformerOperator --stream cache
+
+export MODEL="~/models/nvidia/Hymba-1.5B-Base"
+export OUT_DIR="out/Hymba-1.5B-Base"
+python extract_activations.py --model $MODEL --add_newlines --task class_to_class --k 4 --split demo --out_dir $OUT_DIR --operator HymbaOperator --stream cache
+python extract_activations.py --model $MODEL --add_newlines --task class_to_class_random --k 4 --split demo --out_dir $OUT_DIR --operator HymbaOperator --stream cache
+
+export MODEL="~/models/RWKV/rwkv-6-world-1b6"
+export OUT_DIR="out/rwkv-6-world-1b6"
+python extract_activations.py --model $MODEL --add_newlines --task class_to_class --k 4 --split demo --out_dir $OUT_DIR --operator RWKVOperator --stream cache
+python extract_activations.py --model $MODEL --add_newlines --task class_to_class_random --k 4 --split demo --out_dir $OUT_DIR --operator RWKVOperator --stream cache
+
+export MODEL="~/models/state-spaces/mamba-1.4b-hf"
+export OUT_DIR="out/mamba-1.4b-hf"
+python extract_activations.py --model $MODEL --add_newlines --task class_to_class --k 4 --split demo --out_dir $OUT_DIR --operator MambaOperator --stream cache
+python extract_activations.py --model $MODEL --add_newlines --task class_to_class_random --k 4 --split demo --out_dir $OUT_DIR --operator MambaOperator --stream cache
+
+export MODEL="~/models/Zyphra/Zamba2-1.2B"
+export OUT_DIR="out/Zamba2-1.2B"
+python extract_activations.py --model $MODEL --add_newlines --task class_to_class --k 4 --split demo --out_dir $OUT_DIR --operator ZambaOperator --stream cache
+python extract_activations.py --model $MODEL --add_newlines --task class_to_class_random --k 4 --split demo --out_dir $OUT_DIR --operator ZambaOperator --stream cache
---
+++ ICL/132.82.103.232/virtualenv_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1  
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=16G
+#SBATCH --time=1-00:00
+#SBATCH --account=def-lingjzhu
+
+conda create -n LLM     # change accordingly
+conda activate LLM
+
+cd ~/SlurmScripts/ICL/132.82.103.232
+
+pip install torch==2.5.1
+pip install -r ./requirements.txt
