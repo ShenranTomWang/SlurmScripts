440c6f9b3f9693a740ccb660bdd36b841c4e96eb
shenranw <wsr.20021128@gmail.com>
2025-03-09 15:12:41 -0700

    use short datasets

--- ICL/132.82.103.232/test_custom_save_cache.sh
+++ ICL/132.82.103.232/test_custom_save_cache.sh
@@ -18,29 +18,29 @@ cd /home/tomwang/ICL
 export MODEL="nvidia/Hymba-1.5B-Base"
 export CACHE_DIR="out/Hymba-1.5B-Base"
 export OUT_DIR="out/Hymba-1.5B-Base"
-python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --add_newlines --task class_to_class --k 4 --n_skips 1 --use_demo_cache --save_demo_cache --device $DEVICE
-python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --add_newlines --task class_to_class_random --k 4 --n_skips 1 --use_demo_cache --save_demo_cache --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --add_newlines --task class_to_class_short --k 4 --n_skips 1 --use_demo_cache --save_demo_cache --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --add_newlines --task class_to_class_short_random --k 4 --n_skips 1 --use_demo_cache --save_demo_cache --device $DEVICE
 
 export MODEL="RWKV/rwkv-6-world-1b6"
 export CACHE_DIR="out/rwkv-6-world-1b6"
 export OUT_DIR="out/rwkv-6-world-1b6"
-python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator RWKVOperator --add_newlines --task class_to_class --k 4 --n_skips -1 --use_demo_cache --save_demo_cache --device $DEVICE
-python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator RWKVOperator --add_newlines --task class_to_class_random --k 4 --n_skips -1 --use_demo_cache --save_demo_cache --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator RWKVOperator --add_newlines --task class_to_class_short --k 4 --n_skips -1 --use_demo_cache --save_demo_cache --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator RWKVOperator --add_newlines --task class_to_class_short_random --k 4 --n_skips -1 --use_demo_cache --save_demo_cache --device $DEVICE
 
 # export MODEL="Qwen/Qwen2.5-1.5B"
 # export CACHE_DIR="out/Qwen2.5-1.5B"
 # export OUT_DIR="out/Qwen2.5-1.5B"
-# python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator TransformerOperator --add_newlines --task class_to_class --k 4 --n_skips -1 --use_demo_cache --save_demo_cache --device $DEVICE
-# python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator TransformerOperator --add_newlines --task class_to_class_random --k 4 --n_skips -1 --use_demo_cache --save_demo_cache --device $DEVICE
+# python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator TransformerOperator --add_newlines --task class_to_class_short --k 4 --n_skips -1 --use_demo_cache --save_demo_cache --device $DEVICE
+# python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator TransformerOperator --add_newlines --task class_to_class_short_random --k 4 --n_skips -1 --use_demo_cache --save_demo_cache --device $DEVICE
 
 export MODEL="state-spaces/mamba-1.4b-hf"
 export CACHE_DIR="out/mamba-1.4b-hf"
 export OUT_DIR="out/mamba-1.4b-hf"
-python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --add_newlines --task class_to_class --k 4 --n_skips -1 --use_demo_cache --save_demo_cache --device $DEVICE
-python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --add_newlines --task class_to_class_random --k 4 --n_skips -1 --use_demo_cache --save_demo_cache --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --add_newlines --task class_to_class_short --k 4 --n_skips -1 --use_demo_cache --save_demo_cache --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --add_newlines --task class_to_class_short_random --k 4 --n_skips -1 --use_demo_cache --save_demo_cache --device $DEVICE
 
 export MODEL="Zyphra/Zamba2-1.2B"
 export CACHE_DIR="out/Zamba2-1.2B"
 export OUT_DIR="out/Zamba2-1.2B"
-python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --add_newlines --task class_to_class --k 4 --n_skips 1 --use_demo_cache --save_demo_cache --device $DEVICE
-python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --add_newlines --task class_to_class_random --k 4 --n_skips 1 --use_demo_cache --save_demo_cache --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --add_newlines --task class_to_class_short --k 4 --n_skips 1 --use_demo_cache --save_demo_cache --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --add_newlines --task class_to_class_short_random --k 4 --n_skips 1 --use_demo_cache --save_demo_cache --device $DEVICE
