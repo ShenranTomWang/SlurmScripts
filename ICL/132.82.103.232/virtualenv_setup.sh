e5d5878935573b7e942641e3643289d27fe1a7c1
shenranw <wsr.20021128@gmail.com>
2025-03-09 23:35:38 -0700

    setup should include data scraping

--- ICL/132.82.103.232/virtualenv_setup.sh
+++ ICL/132.82.103.232/virtualenv_setup.sh
@@ -13,3 +13,7 @@ cd /home/tomwang/SlurmScripts/ICL/132.82.103.232
 conda install cmake
 pip install torch==2.5.1
 pip install -r ./requirements.txt
+pip install --no-deps datsets==1.4.0
+cd /home/tomwang/ICL/preprocess
+python _build_gym.py --build --n_proc=40 --do_test
+pip install --no-deps datasets==3.2.0
