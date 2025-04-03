bfd344472c39b6a17189b7f24076fc740f86270c
shenranw <wsr.20021128@gmail.com>
2025-04-03 13:01:01 -0700

    debug

--- ICL/sockeye/virtualenv_setup.sh
+++ ICL/sockeye/virtualenv_setup.sh
@@ -5,15 +5,15 @@
 #SBATCH --time=1-00:00
 #SBATCH --account=def-lingjzhu
 
-module load intel-oneapi-compilers/2023.1.0 python/3.11.6 gcc
-module load cuda/12.4.0
+module load intel-oneapi-compilers/2023.1.0 python/3.11.6
+module load cuda
 export ENVDIR=/scratch/st-jzhu71-1/shenranw/envs/ICL     # change accordingly
-python -m venv $ENVDIR
+python3.11 -m venv $ENVDIR
 source $ENVDIR/bin/activate
 
-pip install torch sentencepiece pandas nbformat tqdm
-pip install transformer_lens tiktoken protobuf ninja einops triton packaging
-pip install notebook mamba-ssm wheel flash-attn causal-conv1d
+pip install torch==2.5.1 triton==3.1.0 wheel sentencepiece pandas nbformat tqdm
+pip install transformer_lens tiktoken protobuf ninja einops packaging
+pip install notebook mamba-ssm flash-attn causal-conv1d
 
 git clone https://github.com/pytorch-labs/attention-gym.git
 cd attention-gym
