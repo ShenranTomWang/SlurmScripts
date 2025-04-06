3c0f48bf0373282a2c56cf6d6ca2b4954f114945
shenranw <wsr.20021128@gmail.com>
2025-04-05 23:21:21 -0700

    experiments for ICL

---
+++ ICL/132.82.103.232/test_custom_rethink_demo.sh
@@ -0,0 +1,69 @@
+conda activate LLM
+
+export TRITON_CACHE_DIR="/home/tomwang/triton_cache"
+export HF_HOME="/home/tomwang/transformers_cache"
+export export CUDA_VISIBLE_DEVICES=3
+export DEVICE="cuda"
+export TOKENIZERS_PARALLELISM=false
+
+cd /home/tomwang/ICL
+
+export MODEL="nvidia/Hymba-1.5B-Base"
+export OUT_DIR="out/Hymba-1.5B-Base"
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo --k 0 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_0_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_25_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_50_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_75_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_random --k 16 --device $DEVICE
+
+export MODEL="RWKV/rwkv-6-world-1b6"
+export OUT_DIR="out/rwkv-6-world-1b6"
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo --k 0 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator RWKVOperator --task rethink_dmeo --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_0_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_25_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_50_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_75_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator RWKVOperator --task rethink_dmeo_random --k 16 --device $DEVICE
+
+export MODEL="Qwen/Qwen2.5-1.5B"
+export OUT_DIR="out/Qwen2.5-1.5BF"
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo --k 0 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator TransformerOperator --task rethink_dmeo --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_0_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_25_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_50_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_75_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator TransformerOperator --task rethink_dmeo_random --k 16 --device $DEVICE
+
+export MODEL="state-spaces/mamba-1.4b-hf"
+export OUT_DIR="out/mamba-1.4b-hf"
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo --k 0 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task rethink_dmeo --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_0_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_25_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_50_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_75_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task rethink_dmeo_random --k 16 --device $DEVICE
+
+export MODEL="AntonV/mamba2-1.3b-hf"
+export OUT_DIR="out/mamba2-1.3b-hf"
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo --k 0 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task rethink_dmeo --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_0_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_25_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_50_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_75_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task rethink_dmeo_random --k 16 --device $DEVICE
+
+export MODEL="Zyphra/Zamba2-1.2B"
+export OUT_DIR="out/Zamba2-1.2B"
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo --k 0 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task rethink_dmeo --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_0_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_25_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_50_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task rethink_demo_75_correct --k 16 --device $DEVICE
+python test_custom.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task rethink_dmeo_random --k 16 --device $DEVICE
