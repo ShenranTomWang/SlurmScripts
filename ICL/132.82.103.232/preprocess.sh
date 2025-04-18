64d269e558a5bae5112b8958e9378e5273555e4c
shenranw <wsr.20021128@gmail.com>
2025-04-18 14:42:26 -0700

    add preprocess for function vectors datasets

--- ICL/132.82.103.232/preprocess.sh
+++ ICL/132.82.103.232/preprocess.sh
@@ -4,3 +4,4 @@ cd /home/tomwang/ICL/preprocess
 
 python ./_build_gym.py --build --n_proc 40 --do_test --task "ALL" --train_k 16 --valid_k 16
 python ./_build_gym.py --build --n_proc 8 --do_test --task "FV" --train_k -1 --valid_k 100
+python ./_build_gym.py --build --n_proc 18 --do_test --task "FV_OG" --train_k -1 --valid_k 100
