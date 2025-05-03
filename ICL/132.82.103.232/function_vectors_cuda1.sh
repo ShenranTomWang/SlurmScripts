274d3f69c97d063b5985f6e1e441d09071dd5f5a
shenranw <wsr.20021128@gmail.com>
2025-05-02 23:01:01 -0700

    debug

--- ICL/132.82.103.232/function_vectors_cuda1.sh
+++ ICL/132.82.103.232/function_vectors_cuda1.sh
@@ -15,8 +15,8 @@ python function_vectors.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Ope
 python ./visualize_fv.py --out_dir $OUT_DIR --seed 100 --task function_vectors_random --load_dir $OUT_DIR
 python function_vectors.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task function_vectors_original_random --device $DEVICE --fv_load_dir $OUT_DIR --seed 100 --use_template AIE
 python ./visualize_fv.py --out_dir $OUT_DIR --seed 100 --task function_vectors_original_random --load_dir $OUT_DIR
-python function_vectors.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task function_vectors_original_incorrect_mapping --device $DEVICE --fv_load_dir $OUT_DIR --seed 100 --use_template AIE
-python ./visualize_fv.py --out_dir $OUT_DIR --seed 100 --task function_vectors_original_incorrect_mapping --load_dir $OUT_DIR
+python function_vectors.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task function_vectors_incorrect_mapping --device $DEVICE --fv_load_dir $OUT_DIR --seed 100 --use_template AIE
+python ./visualize_fv.py --out_dir $OUT_DIR --seed 100 --task function_vectors_incorrect_mapping --load_dir $OUT_DIR
 
 export MODEL="meta-llama/Llama-3.2-1B"
 export OUT_DIR="out/Llama-3.2-1B"
@@ -24,5 +24,5 @@ python function_vectors.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOpe
 python ./visualize_fv.py --out_dir $OUT_DIR --seed 100 --task function_vectors_random --load_dir $OUT_DIR
 python function_vectors.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task function_vectors_original_random --device $DEVICE --fv_load_dir $OUT_DIR --seed 100 --use_template AIE
 python ./visualize_fv.py --out_dir $OUT_DIR --seed 100 --task function_vectors_original_random --load_dir $OUT_DIR
-python function_vectors.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task function_vectors_original_incorrect_mapping --device $DEVICE --fv_load_dir $OUT_DIR --seed 100 --use_template AIE
-python ./visualize_fv.py --out_dir $OUT_DIR --seed 100 --task function_vectors_original_incorrect_mapping --load_dir $OUT_DIR
+python function_vectors.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task function_vectors_incorrect_mapping --device $DEVICE --fv_load_dir $OUT_DIR --seed 100 --use_template AIE
+python ./visualize_fv.py --out_dir $OUT_DIR --seed 100 --task function_vectors_incorrect_mapping --load_dir $OUT_DIR
