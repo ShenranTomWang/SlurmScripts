ebc0a14203a09dbdbcc2b1f5681ab5fd7c83a9e9
shenranw <wsr.20021128@gmail.com>
2025-05-29 18:16:18 -0700

    fixes according to change

--- ICL/132.82.103.232/test_fv_og_removal_ablation.sh
+++ ICL/132.82.103.232/test_fv_og_removal_ablation.sh
@@ -16,34 +16,34 @@ do
     export MODEL="nvidia/Hymba-1.5B-Base"
     export OUT_DIR="out/Hymba-1.5B-Base"
     export LOG_DIR="logs/Hymba-1.5B-Base/$k"
-    python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log.log removal_ablation --p $p
-    python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log_scan.log removal_ablation --p $p --stream scan
-    python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log_attn.log removal_ablation --p $p --stream attn
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log.log zero_ablation --p $p
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log_scan.log zero_ablation --p $p --stream scan
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log_attn.log zero_ablation --p $p --stream attn
 
     export MODEL="Qwen/Qwen2.5-1.5B"
     export OUT_DIR="out/Qwen2.5-1.5B"
     export LOG_DIR="logs/Qwen2.5-1.5B/$k"
-    python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log.log removal_ablation --p $p
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log.log zero_ablation --p $p
 
     export MODEL="meta-llama/Llama-3.2-1B"
     export OUT_DIR="out/Llama-3.2-1B"
     export LOG_DIR="logs/Llama-3.2-1B/$k"
-    python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log.log removal_ablation --p $p
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log.log zero_ablation --p $p
 
     export MODEL="state-spaces/mamba-1.4b-hf"
     export OUT_DIR="out/mamba-1.4b-hf"
     export LOG_DIR="logs/mamba-1.4b-hf/$k"
-    python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log.log removal_ablation --p $p
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log.log zero_ablation --p $p
 
     export MODEL="AntonV/mamba2-1.3b-hf"
     export OUT_DIR="out/mamba2-1.3b-hf"
     export LOG_DIR="logs/mamba2-1.3b-hf/$k"
-    python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log.log removal_ablation --p $p
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log.log zero_ablation --p $p
 
     export MODEL="Zyphra/Zamba2-1.2B"
     export OUT_DIR="out/Zamba2-1.2B"
     export LOG_DIR="logs/Zamba2-1.2B/$k"
-    python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log.log removal_ablation --p $p
-    python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log_scan.log removal_ablation --p $p --stream scan
-    python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log_attn.log removal_ablation --p $p --stream attn
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log.log zero_ablation --p $p
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log_scan.log zero_ablation --p $p --stream scan
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors_original --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original_removal_ablation/$p/log_attn.log zero_ablation --p $p --stream attn
 done
