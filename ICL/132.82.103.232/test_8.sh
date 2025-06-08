6e7e3d67b5097345d08faa780443f2ced6aac303
shenranw <wsr.20021128@gmail.com>
2025-06-08 12:29:04 -0700

    full script

--- ICL/132.82.103.232/test_8.sh
+++ ICL/132.82.103.232/test_8.sh
@@ -30,6 +30,10 @@ python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task
 python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors_original_random --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_random.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors_original_incorrect_mapping --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_incorrect_mapping_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors_original_incorrect_mapping --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_incorrect_mapping.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors_original_0_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_0_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors_original_25_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_25_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors_original_50_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_50_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors_original_75_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_75_correct.log
 
 export MODEL="Qwen/Qwen2.5-1.5B"
 export OUT_DIR="out/Qwen2.5-1.5B"
@@ -50,6 +54,10 @@ python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task
 python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task function_vectors_original_random --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_random.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task function_vectors_original_incorrect_mapping --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_incorrect_mapping_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task function_vectors_original_incorrect_mapping --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_incorrect_mapping.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task function_vectors_original_0_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_0_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task function_vectors_original_25_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_25_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task function_vectors_original_50_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_50_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task function_vectors_original_75_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_75_correct.log
 
 export MODEL="meta-llama/Llama-3.2-1B"
 export OUT_DIR="out/Llama-3.2-1B"
@@ -70,6 +78,10 @@ python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task
 python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task function_vectors_original_random --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_random.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task function_vectors_original_incorrect_mapping --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_incorrect_mapping_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task function_vectors_original_incorrect_mapping --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_incorrect_mapping.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task function_vectors_original_0_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_0_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task function_vectors_original_25_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_25_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task function_vectors_original_50_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_50_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task function_vectors_original_75_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_75_correct.log
 
 export MODEL="google/gemma-3-1b-pt"
 export OUT_DIR="out/gemma-3-1b-pt"
@@ -90,6 +102,10 @@ python test.py --model $MODEL --out_dir $OUT_DIR --operator ForwardWrapperTransf
 python test.py --model $MODEL --out_dir $OUT_DIR --operator ForwardWrapperTransformerOperator --task function_vectors_original_random --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_random.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator ForwardWrapperTransformerOperator --task function_vectors_original_incorrect_mapping --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_incorrect_mapping_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator ForwardWrapperTransformerOperator --task function_vectors_original_incorrect_mapping --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_incorrect_mapping.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator ForwardWrapperTransformerOperator --task function_vectors_original_0_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_0_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator ForwardWrapperTransformerOperator --task function_vectors_original_25_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_25_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator ForwardWrapperTransformerOperator --task function_vectors_original_50_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_50_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator ForwardWrapperTransformerOperator --task function_vectors_original_75_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_75_correct.log
 
 export MODEL="state-spaces/mamba-1.4b-hf"
 export OUT_DIR="out/mamba-1.4b-hf"
@@ -110,6 +126,10 @@ python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task
 python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task function_vectors_original_random --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_random.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task function_vectors_original_incorrect_mapping --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_incorrect_mapping_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task function_vectors_original_incorrect_mapping --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_incorrect_mapping.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task function_vectors_original_0_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_0_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task function_vectors_original_25_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_25_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task function_vectors_original_50_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_50_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task function_vectors_original_75_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_75_correct.log
 
 export MODEL="AntonV/mamba2-1.3b-hf"
 export OUT_DIR="out/mamba2-1.3b-hf"
@@ -130,6 +150,10 @@ python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --tas
 python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task function_vectors_original_random --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_random.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task function_vectors_original_incorrect_mapping --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_incorrect_mapping_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task function_vectors_original_incorrect_mapping --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_incorrect_mapping.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task function_vectors_original_0_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_0_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task function_vectors_original_25_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_25_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task function_vectors_original_50_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_50_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task function_vectors_original_75_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_75_correct.log
 
 export MODEL="Zyphra/Zamba2-1.2B"
 export OUT_DIR="out/Zamba2-1.2B"
@@ -150,3 +174,7 @@ python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task
 python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors_original_random --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_random.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors_original_incorrect_mapping --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_incorrect_mapping_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors_original_incorrect_mapping --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_incorrect_mapping.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors_original_0_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_0_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors_original_25_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_25_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors_original_50_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_50_correct.log
+python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors_original_75_correct --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors_original/log_75_correct.log
