6bbd45bf24129f74cfa50763b63f1167f1a19c1f
shenranw <wsr.20021128@gmail.com>
2025-06-15 13:15:01 -0700

    steering script

---
+++ ICL/132.82.103.232/test_steer_incorrect_mapping.sh
@@ -0,0 +1,39 @@
+export TRITON_CACHE_DIR="/home/tomwang/triton_cache"
+export HF_HOME="/home/tomwang/transformers_cache"
+export CUDA_VISIBLE_DEVICES=0
+export DEVICE="cuda"
+export TOKENIZERS_PARALLELISM=false
+
+cd /home/tomwang/ICL
+
+export k=16
+
+export MODEL="nvidia/Hymba-1.5B-Base"
+export OUT_DIR="out/Hymba-1.5B-Base"
+export LOG_DIR="logs/Hymba-1.5B-Base/$k"
+python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors/log_steer_incorrect_mapping.log steer --p 0.2
+
+export MODEL="Qwen/Qwen2.5-1.5B"
+export OUT_DIR="out/Qwen2.5-1.5B"
+export LOG_DIR="logs/Qwen2.5-1.5B/$k"
+python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task function_vectors --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors/log_steer_incorrect_mapping.log steer --p 0.2
+
+export MODEL="meta-llama/Llama-3.2-1B"
+export OUT_DIR="out/Llama-3.2-1B"
+export LOG_DIR="logs/Llama-3.2-1B/$k"
+python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task function_vectors --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors/log_steer_incorrect_mapping.log steer --p 0.2
+
+export MODEL="state-spaces/mamba-1.4b-hf"
+export OUT_DIR="out/mamba-1.4b-hf"
+export LOG_DIR="logs/mamba-1.4b-hf/$k"
+python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task function_vectors --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors/log_steer_incorrect_mapping.log steer --p 0.2
+
+export MODEL="AntonV/mamba2-1.3b-hf"
+export OUT_DIR="out/mamba2-1.3b-hf"
+export LOG_DIR="logs/mamba2-1.3b-hf/$k"
+python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task function_vectors --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors/log_steer_incorrect_mapping.log steer --p 0.2
+
+export MODEL="Zyphra/Zamba2-1.2B"
+export OUT_DIR="out/Zamba2-1.2B"
+export LOG_DIR="logs/Zamba2-1.2B/$k"
+python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors --k $k --device $DEVICE --log_file $LOG_DIR/function_vectors/log_steer_incorrect_mapping.log steer --p 0.2
