66850c26d9e283a1d3ca417e745aac1e42f235fa
shenranw <wsr.20021128@gmail.com>
2025-06-21 14:18:01 -0700

    standard ablation

--- ICL/132.82.103.232/test_classification_steer_incorrect_mapping.sh
+++ ICL/132.82.103.232/test_classification_steer_incorrect_mapping.sh
@@ -11,42 +11,36 @@ export k=16
 export MODEL="nvidia/Hymba-1.5B-Base"
 export OUT_DIR="out/Hymba-1.5B-Base"
 export LOG_DIR="logs/Hymba-1.5B-Base/$k"
-python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task classification --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task classification_incorrect_mapping --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log_incorrect_mapping_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task classification_incorrect_mapping --k $k --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log.log
 
 export MODEL="Qwen/Qwen2.5-1.5B"
 export OUT_DIR="out/Qwen2.5-1.5B"
 export LOG_DIR="logs/Qwen2.5-1.5B/$k"
-python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task classification --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task classification_incorrect_mapping --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log_incorrect_mapping_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task classification_incorrect_mapping --k $k --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log.log
 
 export MODEL="meta-llama/Llama-3.2-1B"
 export OUT_DIR="out/Llama-3.2-1B"
 export LOG_DIR="logs/Llama-3.2-1B/$k"
-python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task classification --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task classification_incorrect_mapping --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log_incorrect_mapping_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task classification_incorrect_mapping --k $k --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log.log
 
 export MODEL="state-spaces/mamba-1.4b-hf"
 export OUT_DIR="out/mamba-1.4b-hf"
 export LOG_DIR="logs/mamba-1.4b-hf/$k"
-python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task classification --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task classification_incorrect_mapping --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log_incorrect_mapping_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task classification_incorrect_mapping --k $k --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log.log
 
 export MODEL="AntonV/mamba2-1.3b-hf"
 export OUT_DIR="out/mamba2-1.3b-hf"
 export LOG_DIR="logs/mamba2-1.3b-hf/$k"
-python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task classification --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task classification_incorrect_mapping --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log_incorrect_mapping_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task classification_incorrect_mapping --k $k --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log.log
 
 export MODEL="Zyphra/Zamba2-1.2B"
 export OUT_DIR="out/Zamba2-1.2B"
 export LOG_DIR="logs/Zamba2-1.2B/$k"
-python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task classification --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task classification_incorrect_mapping --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log_incorrect_mapping_no_demo.log
 python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task classification_incorrect_mapping --k $k --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/log.log
 
@@ -55,30 +49,36 @@ do
     export MODEL="nvidia/Hymba-1.5B-Base"
     export OUT_DIR="out/Hymba-1.5B-Base"
     export LOG_DIR="logs/Hymba-1.5B-Base/$k"
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task classification --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/$p/log_no_demo.log steer --p $p --target ""
     python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task classification --k $k --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/$p/log.log steer --p $p
 
     export MODEL="Qwen/Qwen2.5-1.5B"
     export OUT_DIR="out/Qwen2.5-1.5B"
     export LOG_DIR="logs/Qwen2.5-1.5B/$k"
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task classification --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/$p/log_no_demo.log steer --p $p --target ""
     python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task classification --k $k --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/$p/log.log steer --p $p
 
     export MODEL="meta-llama/Llama-3.2-1B"
     export OUT_DIR="out/Llama-3.2-1B"
     export LOG_DIR="logs/Llama-3.2-1B/$k"
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task classification --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/$p/log_no_demo.log steer --p $p --target ""
     python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task classification --k $k --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/$p/log.log steer --p $p
 
     export MODEL="state-spaces/mamba-1.4b-hf"
     export OUT_DIR="out/mamba-1.4b-hf"
     export LOG_DIR="logs/mamba-1.4b-hf/$k"
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task classification --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/$p/log_no_demo.log steer --p $p --target ""
     python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task classification --k $k --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/$p/log.log steer --p $p
 
     export MODEL="AntonV/mamba2-1.3b-hf"
     export OUT_DIR="out/mamba2-1.3b-hf"
     export LOG_DIR="logs/mamba2-1.3b-hf/$k"
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task classification --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/$p/log_no_demo.log steer --p $p --target ""
     python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task classification --k $k --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/$p/log.log steer --p $p
 
     export MODEL="Zyphra/Zamba2-1.2B"
     export OUT_DIR="out/Zamba2-1.2B"
     export LOG_DIR="logs/Zamba2-1.2B/$k"
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task classification --k 0 --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/$p/log_no_demo.log steer --p $p --target ""
     python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task classification --k $k --device $DEVICE --log_file $LOG_DIR/classification/steer_incorrect_mapping/$p/log.log steer --p $p
 done
