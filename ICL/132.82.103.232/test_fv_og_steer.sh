d851106d7eb49d34eb871951d695c220ab6e0875
shenranw <wsr.20021128@gmail.com>
2025-06-24 09:25:45 -0700

    mean pooled experiments

--- ICL/132.82.103.232/test_fv_og_steer.sh
+++ ICL/132.82.103.232/test_fv_og_steer.sh
@@ -14,29 +14,35 @@ do
     export OUT_DIR="out/Hymba-1.5B-Base"
     export LOG_DIR="logs/Hymba-1.5B-Base/$k"
     python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors_original --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/steer/$p/log_no_demo.log steer --p $p
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator HymbaOperator --task function_vectors_original --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/steer/$p/log_no_demo_mean.log steer --p $p --mean_pool
 
     export MODEL="Qwen/Qwen2.5-1.5B"
     export OUT_DIR="out/Qwen2.5-1.5B"
     export LOG_DIR="logs/Qwen2.5-1.5B/$k"
     python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task function_vectors_original --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/steer/$p/log_no_demo.log steer --p $p
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator Qwen2Operator --task function_vectors_original --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/steer/$p/log_no_demo_mean.log steer --p $p --mean_pool
 
     export MODEL="meta-llama/Llama-3.2-1B"
     export OUT_DIR="out/Llama-3.2-1B"
     export LOG_DIR="logs/Llama-3.2-1B/$k"
     python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task function_vectors_original --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/steer/$p/log_no_demo.log steer --p $p
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator LlamaOperator --task function_vectors_original --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/steer/$p/log_no_demo_mean.log steer --p $p --mean_pool
 
     export MODEL="state-spaces/mamba-1.4b-hf"
     export OUT_DIR="out/mamba-1.4b-hf"
     export LOG_DIR="logs/mamba-1.4b-hf/$k"
     python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task function_vectors_original --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/steer/$p/log_no_demo.log steer --p $p
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator MambaOperator --task function_vectors_original --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/steer/$p/log_no_demo_mean.log steer --p $p --mean_pool
 
     export MODEL="AntonV/mamba2-1.3b-hf"
     export OUT_DIR="out/mamba2-1.3b-hf"
     export LOG_DIR="logs/mamba2-1.3b-hf/$k"
     python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task function_vectors_original --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/steer/$p/log_no_demo.log steer --p $p
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator Mamba2Operator --task function_vectors_original --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/steer/$p/log_no_demo_mean.log steer --p $p --mean_pool
 
     export MODEL="Zyphra/Zamba2-1.2B"
     export OUT_DIR="out/Zamba2-1.2B"
     export LOG_DIR="logs/Zamba2-1.2B/$k"
     python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors_original --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/steer/$p/log_no_demo.log steer --p $p
+    python test.py --model $MODEL --out_dir $OUT_DIR --operator ZambaOperator --task function_vectors_original --k 0 --device $DEVICE --log_file $LOG_DIR/function_vectors_original/steer/$p/log_no_demo_mean.log steer --p $p --mean_pool
 done
