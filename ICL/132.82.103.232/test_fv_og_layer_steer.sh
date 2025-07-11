109617977ddc752229b14ec8cfce75071f7cc6c7
shenranw <wsr.20021128@gmail.com>
2025-07-11 11:49:16 -0700

    layer level steering

---
+++ ICL/132.82.103.232/test_fv_og_layer_steer.sh
@@ -0,0 +1,27 @@
+export TRITON_CACHE_DIR="/home/tomwang/triton_cache"
+export HF_HOME="/home/tomwang/transformers_cache"
+export CUDA_VISIBLE_DEVICES=3
+export DEVICE="cuda"
+export TOKENIZERS_PARALLELISM=false
+
+cd /home/tomwang/ICL
+
+export k=16
+
+export MODEL="nvidia/Hymba-1.5B-Base"
+python layer_steer.py --model $MODEL --operator HymbaOperator --task function_vectors_original --device $DEVICE --mean_pool
+
+export MODEL="Qwen/Qwen2.5-1.5B"
+python layer_steer.py --model $MODEL --operator QwenOperator --task function_vectors_original --device $DEVICE --mean_pool
+
+export MODEL="meta-llama/Llama-3.2-1B"
+python layer_steer.py --model $MODEL --operator LlamaOperator --task function_vectors_original --device $DEVICE --mean_pool
+
+export MODEL="state-spaces/mamba-1.4b-hf"
+python layer_steer.py --model $MODEL --operator MambaOperator --task function_vectors_original --device $DEVICE --mean_pool
+
+export MODEL="AntonV/mamba2-1.3b-hf"
+python layer_steer.py --model $MODEL --operator Mamba2Operator --task function_vectors_original --device $DEVICE --mean_pool
+
+export MODEL="Zyphra/Zamba2-1.2B"
+python layer_steer.py --model $MODEL --operator ZambaOperator --task function_vectors_original --device $DEVICE --mean_pool
